48724de02a67ffd6a3bd05d356a882345e3f3182
Ali Habib <ali.m.habib@hotmail.com>
2022-07-31 23:22:54 +0200

    location-storage-consumer

---
+++ modules/location-storage-consumer/app/services.py
@@ -0,0 +1,37 @@
+    
+from datetime import datetime, timedelta
+import os
+from typing import Dict, List
+
+from app import db
+from app.models import Location
+from app.schemas import LocationSchema
+from geoalchemy2.functions import ST_AsText, ST_Point
+
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+ 
+DB_USERNAME = os.environ["DB_USERNAME"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_NAME = os.environ["DB_NAME"]
+
+engine = sa.create_engine( f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
+class LocationService:
+    @staticmethod
+    def create(location: Dict) -> Location:
+        validation_results: Dict = LocationSchema().validate(location)
+        if validation_results: 
+            raise Exception(f"Invalid payload: {validation_results}")
+
+        new_location = Location()
+        new_location.person_id = location["person_id"]
+        new_location.creation_time = location["creation_time"]
+        new_location.coordinate = ST_Point(location["latitude"], location["longitude"])
+        factory = orm.sessionmaker(bind=engine)
+        session = factory()
+        session.add(new_location)
+        session.commit()
+        return new_location 
---
+++ modules/location-storage-consumer/app/start.py
@@ -0,0 +1,45 @@
+import json
+import socket
+import sys 
+from confluent_kafka import Consumer
+from confluent_kafka.error import KafkaError,KafkaException
+
+from app.services import LocationService
+ 
+
+
+global running
+running = True
+conf = {"bootstrap.servers": "kafka-service:9092", "client.id": socket.gethostname()}
+
+def basic_consume_loop(consumer, topics):
+    try:
+        consumer.subscribe(topics)
+
+        while running:
+            msg = consumer.poll(timeout=1.0)
+            if msg is None: continue
+
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    # End of partition event
+                    sys.stderr.write('%% %s [%d] reached end at offset %d\n' %
+                                     (msg.topic(), msg.partition(), msg.offset()))
+                elif msg.error():
+                    raise KafkaException(msg.error())
+            else:
+                LocationService.create(json.loads(msg.value().decode('utf-8')))
+    finally:
+        # Close down consumer to commit final offsets.
+        consumer.close()
+
+def shutdown():
+    running = False
+
+def serve():
+    running = True
+    c = Consumer(conf)
+    basic_consume_loop(c,"Location_Creation")
+
+if __name__ == "__main__":
+    serve()
